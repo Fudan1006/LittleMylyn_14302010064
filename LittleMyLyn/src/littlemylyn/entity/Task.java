3555636c8abae8619ed9ee3718f550bf047faaf3
zukihxy <14302010050@fudan.edu.cn>
2016-05-26 10:41:27 +0800

    add label for related class node

--- LittleMyLyn/src/littlemylyn/entity/Task.java
+++ LittleMyLyn/src/littlemylyn/entity/Task.java
@@ -5,15 +5,14 @@ import org.eclipse.jface.viewers.TreeNode;
 public class Task extends Node{
 	public Node name = new Node();
 	public Node state = new Node();
-	//public static String[] states = {"new", "activated", "finished"};
 	public Node category = new Node();
-	//public static String[] categories = {"debug", "new feature", "refactor"}; 
 	public Node relatedClass = new Node();
 	
 	public Task(String n, String s, String c) {
 		name.setName(n);
 		state.setName(s);
 		category.setName(c);
+		relatedClass.setName("Related Class("+relatedClass.getChildren().size()+")");
 		name.addChild(state);
 		name.addChild(category);
 		name.addChild(relatedClass);
